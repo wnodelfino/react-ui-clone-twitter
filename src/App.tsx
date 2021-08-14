0d33d9de7b590ab72e1f50d7a5b2f22aefd0491f
Wellynton Delfino <wnodelfino@gmail.com>
2021-08-13 21:09:45 -0300

    Ajustando arquico App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Layout from './components/Layout';
 import GlobalStyles from './styles/GlobalStyles';
 
+// eslint-disable-next-line
 function App() {
   return (
     <>
